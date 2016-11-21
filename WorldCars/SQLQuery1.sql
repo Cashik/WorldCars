08f868696f246fa843a02a863c418262a999b0ff
Alexandr <ovdienkoalexandr@mail.ru>
2016-11-21 17:15:56 +0300

    Триггер для комментариев(переподсчет рейтинга машины)

---
+++ WorldCars/SQLQuery1.sql
@@ -0,0 +1,15 @@
+﻿GO
+DROP TRIGGER RatingUpdater;
+go
+Create TRIGGER RatingUpdater  
+ON Comment
+AFTER DELETE,UPDATE,INSERT
+AS 
+
+UPDATE CarInfo 
+Set CarInfo.rating= (
+select AVG(C.rating) 
+from Comment as C
+where C.car_info_id = (select car_info_id from inserted,deleted)
+)
+where Id = (select car_info_id from inserted,deleted);
